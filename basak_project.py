29724f59cd7f75e45c80f8f3015fbe77ea545e5f
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2020-12-28 19:18:37 +0300

    Update basak_project.py
    I will update my projects when i improve them.

--- basak_project.py
+++ basak_project.py
@@ -26,9 +26,11 @@ if(re.match(regex, ad)is None):
 
 def explanation(x):
 
-    code = [100,101,102,200,201,202,203,204,205,206,207,210,300,301,302,303,304,305,307,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,422,423,424,500,501,502,503,504,505]
-    message = ["Continue","Switching Protocols","Proccessing","OK","Created","Accepted", "Non-Authoritative Information","No Content", "Reset Content","Partial Content","Multi-Status","Content Different","Multiple Choises","Moved Permanently","See Other","Not Modified","Use Proxy","Temporary Redirect","Bad Request","Unauthorized","Payment Required","Forbidden","Not Found","Nonaccess","Not Acceptable","Needed Proxy","Time-out","Conflict","Gone","Length Required","Precondition Failed","Request Entity Too Large","Request-URI Too Long","Unsupported Media Type","Requested Range Unstatifiable","Expectation Failed","Unprocessable Entity","Loced","Method Failure","Internal Server Error","Unapplied","Bad Gateway","No Service","Gateway Timeout","HTTP Version not Supported"]
-    for i in range(0,len(code)):
+     code = [100,101,102,200,201,202,203,204,205,206,207,210,300,301,302,303,304,305,307,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,422,423,424,500,501,502,503,504,505]
+     message = ["Continue","Switching Protocols","Proccessing","OK","Created","Accepted", "Non-Authoritative Information","No Content", "Reset Content","Partial Content","Multi-Status","Content Different","Multiple Choises","Moved Permanently","Multiple Temporarily","See Other","Not Modified","Use Proxy","Temporary Redirect","Bad Request","Unauthorized","Payment Required","Forbidden","Not Found","Nonaccess","Not Acceptable","Needed Proxy","Time-out","Conflict","Gone","Length Required","Precondition Failed","Request Entity Too Large","Request-URI Too Long","Unsupported Media Type","Requested Range Unstatifiable","Expectation Failed","Unprocessable Entity","Loced","Method Failure","Internal Server Error","Unapplied","Bad Gateway","No Service","Gateway Timeout","HTTP Version not Supported"]
+     
+
+     for i in range(0,len(code)):
 
         if(x==code[i]):
 
