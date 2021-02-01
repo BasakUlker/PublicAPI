cb406ce69264ebc2cccd568b3a19fc743cc5c884
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-02-01 17:17:25 +0300

    Update basak_project.py

--- basak_project.py
+++ basak_project.py
@@ -8,31 +8,48 @@ while(1==1):
     ad = input("Please, paste your http address: ")
 
     
-    a = {"h","t","p","s"}
-    b = list(ad)
-    c = set(b[:4])
-    inter = a.intersection(c)
-    if(len(inter)!=3):
+   
+   # a = {"h","t","p","s"}
+    #b = list(ad)
+    #c = set(b[:4])
+   # inter = a.intersection(c)
+    #if(len(inter)!=3):
 
-        ad = "https://"+ad
+     #   ad = "https://"+ad
         #print(ad)
-    b.clear()
+    #b.clear()
     k = 1
 
 #https://qastack.info.tr/programming/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
 
-    regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|' #localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    def reg(ad):
+        regex = re.compile(
+                r'^(?:http|ftp)s?://' # http:// or https://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                r'localhost|' #localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                r'(?::\d+)?' # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        return regex
 
 #print(re.match(regex, ad) is not None) # True-False
-    if(re.match(regex, ad)is None):
-        print('Address/netmask is invalid for IPv4:', ad)
-        k=0
+    if(re.match(reg(ad), ad)is None):
+            r1 = re.match(ad,"https://")
+            r2 = re.match(ad,"http://")
+            if(r1 is None and r2 is None):
+                ad = "https://"+ad
+
+
+    if(re.match(reg(ad),ad)is None):
+
+
+
+
+
+
+            print('Address/netmask is invalid for IPv4:', ad)
+            k=0
 
 
 
