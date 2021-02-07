7933d99fd68cf3dd6992a6191b20c171243e80d5
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-02-07 14:19:03 +0300

    Update basak_project.py

--- basak_project.py
+++ basak_project.py
@@ -1,40 +1,49 @@
 import requests
 import ipaddress
 import re
+from requests.exceptions import HTTPError
 
 while(1==1):
 
-    x=0 
+    x=0
     ad = input("Please, paste your http address: ")
 
-    
-    a = {"h","t","p","s"}
-    b = list(ad)
-    c = set(b[:4])
-    inter = a.intersection(c)
-    if(len(inter)!=3):
 
-        ad = "https://"+ad
+   # a = {"h","t","p","s"}
+    #b = list(ad)
+    #c = set(b[:4])
+   # inter = a.intersection(c)
+    #if(len(inter)!=3):
+
+     #   ad = "https://"+ad
         #print(ad)
-    b.clear()
+    #b.clear()
     k = 1
 
 #https://qastack.info.tr/programming/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
+    def reg(ad):
+        regex = re.compile(
+                r'^(?:http|ftp)s?://' # http:// or https://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                r'localhost|' #localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                r'(?::\d+)?' # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|' #localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return regex
 
 #print(re.match(regex, ad) is not None) # True-False
-    if(re.match(regex, ad)is None):
-        print('Address/netmask is invalid for IPv4:', ad)
-        k=0
+    if(re.match(reg(ad), ad)is None):
+            r1 = re.match(ad,"https://")
+            r2 = re.match(ad,"http://")
+            if(r1 is None and r2 is None):
+                ad = "https://"+ad
 
 
+    if(re.match(reg(ad),ad)is None):
+
+            print('Address/netmask is invalid for IPv4:', ad)
+            k=0
 
     def explanation(x):
 
@@ -61,57 +70,102 @@ while(1==1):
             key = input("A key for POST option: ")
             value = input("A value for POST option: ")
 
-            adpost = requests.post(ad, data = {key:value})
-    
-            print(adpost)
-            print(explanation(adpost.status_code))
-    
-        if(x == 2):
+            try:
+                adpost = requests.post(ad, data = {key:value})
+                adpost.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adpost)
+                print(explanation(adpost.status_code))
+         if(x == 2):
 
 
             key = input("A key for PUT option: ")
             value = input("A value for PUT option: ")
-
-            adput = requests.put(ad, data = {key:value})
-            print(adput)
-            print(explanation(adput.status_code))
+            try:
+                adput = requests.put(ad, data = {key:value})
+                adput.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adput)
+                print(explanation(adput.status_code))
 
         if(x == 3):
+            try:
 
-            addel = requests.delete(ad)
-            print(addel)
-            print(explanation(addel.status_code))
+                addel = requests.delete(ad)
+                addel.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(addel)
+                print(explanation(addel.status_code))
         if(x == 4):
 
-            adhead = requests.head(ad)
-
-            print(adhead)
-            print(explanation(adhead.status_code))
+            try:
+                adhead = requests.head(ad)
+                adhead.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adhead)
+                print(explanation(adhead.status_code))
 
         if(x == 5):
 
 
             key = input("A key for PATCH option: ")
             value = input("A value for PATCH option: ")
+            try:
 
-            adpatch = requests.patch(ad, data ={key:value})
-            print(adpatch)
-            print(explanation(adpatch.status_code))
+                adpatch = requests.patch(ad, data ={key:value})
+                adpatch.raise_for_status()
+            
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adpatch)
+                print(explanation(adpatch.status_code))
 
 
 
 
         if(x == 6):
+            try:
 
-            adoptions = requests.options(ad)
-            print(adoptions)
-            print(explanation(adoptions.status_code))
-    
+                adoptions = requests.options(ad)
+                adoptions.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adoptions)
+                print(explanation(adoptions.status_code))
+
         if(x == 7):
-
-            adget = requests.get(ad)
-            print(adget)
-            print(explanation(adget.status_code))
+            try:
+                adget = requests.get(ad)
+                adget.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                print(adget)
+                print(explanation(adget.status_code))
         if(x==8):
             break
 
@@ -122,3 +176,4 @@ while(1==1):
     if(x==0):
         break
 
+                               
