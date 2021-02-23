5e1ce5fee8ac5ccac718a5afc849067887baea47
BasakUlker <basakulker2@gmail.com>
2021-02-23 21:04:49 +0300

    Deleted api key

--- movieDatabase.py
+++ movieDatabase.py
@@ -11,7 +11,7 @@ payload = {
 } 
 
 headers = {
-	"x-rapidapi-key": "12f443f6f9msh31241e38a337832p1c38aajsn6fe394aeeb79",
+	"x-rapidapi-key": "",
 	"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
 }
 
