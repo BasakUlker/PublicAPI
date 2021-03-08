9b403084bd8851a6501d8881af8e086f21204d52
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-03-08 19:54:54 +0300

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,41 @@
+#This program is listing most popular movies.
+
+import requests
+import json
+
+def returnTitle(x):
+
+    tx = x[0]
+    for i in range(1,len(x)):
+        tx = tx + x[i]
+    url = "https://imdb8.p.rapidapi.com/title/get-plots"
+
+    querystring = {"tconst": tx}
+
+    headers = {
+        'x-rapidapi-key': "12f443f6f9msh31241e38a337832p1c38aajsn6fe394aeeb79",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = json.loads(response.text)
+    print(result["base"]["title"])
+    return 0
+
+url = "https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre"
+
+querystring = {"genre":"/chart/popular/genre/adventure"}
+
+headers = {
+    'x-rapidapi-key': "12f443f6f9msh31241e38a337832p1c38aajsn6fe394aeeb79",
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+result = json.loads(response.text)
+#print(json.dumps(result,indent = "\n", sort_keys = True))
+for i in range(0, len(result)):
+    arr = list(result[i])
+    q = len(arr)-1
+    returnTitle(arr[7:q])
+
